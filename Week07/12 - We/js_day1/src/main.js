b97c38761e58dce6c12ca98458bc8828edc24f4c
Tunoc <random959dude@gmail.com>
2020-02-12 13:23:18 +0100

    .

---
+++ Week07/12 - We/js_day1/src/main.js
@@ -0,0 +1,23 @@
+console.log("Hello World");
+
+const NUMBERS = [1,2,6,7];
+
+
+//NUMBERS.forEach(function(n){
+//    console.log(n);
+//});
+
+
+function logger(n){
+    console.log(n);
+};
+NUMBERS.forEach(logger);
+
+
+
+
+
+
+
+
+
